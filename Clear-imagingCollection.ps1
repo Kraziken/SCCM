0686da90d17caa9d6c7c6a263db1edf75ada7c24
Kraziken <40367834+Kraziken@users.noreply.github.com>
2019-03-18 14:47:45 -0700

    Add files via upload

---
+++ Clear-imagingCollection.ps1
@@ -0,0 +1,26 @@
+﻿$CMSearch='Installation - CM*'
+$BladeSearch='*blades*'
+$CMGroup=Get-CMDeviceCollection|Where-Object -filterscript {$_.name -like $CMSearch}|Select-Object Name,CollectionID
+$BladeGroup=Get-CMDeviceCollection|Where-Object -filterscript {$_.name -like $BladeSearch}|Select-Object Name,CollectionID
+$CMGroup
+$Bladegroup
+$Collections=($CMGroup + $BladeGroup).collectionid
+
+foreach ($Collection in $Collections)
+    {
+    get-cmdevice -CollectionID $Collection|Where-Object clientactivestatus -eq 1
+    
+    }
+
+
+
+<#
+foreach ($Collection in $Collections)
+    {
+    $servers=get-cmdevice -CollectionID $Collection|Where-Object clientactivestatus -eq 1|%{$_.name}
+    Remove-CMDeviceCollectionDirectMembershipRule -ResourceId (get-cmdevice -name $server).resourceid -collectionid $Collection
+    }
+
+
+#>
+
